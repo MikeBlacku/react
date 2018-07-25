a21e029da0e7db2e9e498a5d50422092a753282b
Vsevolod <mikeblacku@gmail.com>
2018-07-25 17:45:12 +0300

    Add Footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Header from '../Header';
 import Body from '../Body';
+import Footer from '../Footer';
 
 export default () => (
   <div>
     <Header />
     <Body />
+    <Footer />
   </div>
 );
