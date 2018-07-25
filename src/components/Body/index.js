70f2caa19b2b67527a87a22c0ed70e48bcdf1d8e
Anton Shamyshev <saa33-saa33@yandex.ru>
2018-07-25 18:23:12 +0300

    Add login greeting

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,3 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => <div>Body</div>;
+class Body extends Component {
+  constructor() {
+    super();
+    this.state = { login: '' };
+  }
+
+  onInputChange = e => {
+    this.setState({ login: e.target.value });
+  };
+
+  onLogIn = () => {
+    alert(`Hello, ${this.state.login}`);
+    this.setState({ login: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        Login:{' '}
+        <input
+          type="text"
+          value={this.state.login}
+          onChange={this.onInputChange}
+        />
+        <br />
+        <button onClick={this.onLogIn}>Войти</button>
+      </div>
+    );
+  }
+}
+
+export default Body;
